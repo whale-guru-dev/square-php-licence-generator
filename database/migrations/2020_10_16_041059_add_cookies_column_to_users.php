a2ae262d4846ab9f26c66809b761cafa13b2d02c
GOD <masterionic@gmail.com>
2020-10-24 12:22:00 +0800

    update db

--- database/migrations/2020_10_16_041059_add_cookies_column_to_users.php
+++ database/migrations/2020_10_16_041059_add_cookies_column_to_users.php
@@ -14,7 +14,7 @@ class AddCookiesColumnToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('cookies');
+            $table->string('cookies')->nullable();
         });
     }
 
