cc3a21f29edc1e3adb5ad49f07e71edfc008e3c2
GOD <masterionic@gmail.com>
2020-10-28 23:41:20 +0800

    change

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -9,10 +9,12 @@
 
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('shopifybot/img/favicon.jpg')}}">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+          integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -21,7 +23,8 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
     <![endif]-->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <link rel="stylesheet" type="text/css" href="{{asset('assets/user/css/style.css')}}">
 </head>
@@ -39,15 +42,20 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="{{route('user.home')}}">
-                    <img src="{{asset('shopifybot/img/logo.png')}}" style="max-height: 160px; max-width: 100px;" class="img-responsive">
+                    <img src="{{asset('shopifybot/img/logo.png')}}" style="max-height: 160px; max-width: 100px;"
+                         class="img-responsive">
                 </a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="@if(request()->path() == 'home') active @endif"><a href="{{route('user.home')}}">Home</a></li>
-                    <li class="@if(request()->path() == 'profile') active @endif"><a href="{{route('user.profile')}}">Profile</a></li>
-                    <li class="@if(request()->path() == 'plans') active @endif"><a href="{{route('user.plan')}}">Plans</a></li>
-                    <li class="@if(request()->path() == 'bot') active @endif"><a href="{{route('user.bot')}}"> Bot</a></li>
+                    <li class="@if(request()->path() == 'home') active @endif"><a href="{{route('user.home')}}">Home</a>
+                    </li>
+                    <li class="@if(request()->path() == 'profile') active @endif"><a href="{{route('user.profile')}}">Profile</a>
+                    </li>
+                    <li class="@if(request()->path() == 'plans') active @endif"><a
+                            href="{{route('user.plan')}}">Plans</a></li>
+                    <li class="@if(request()->path() == 'bot') active @endif"><a href="{{route('user.bot')}}"> Bot</a>
+                    </li>
                     <li><a href="{{route('logout')}}">Logout</a></li>
                 </ul>
             </div><!--/.nav-collapse -->
@@ -56,7 +64,10 @@
 
     <div class="container">
         <div class="row text-center">
-            <img class="img-fluid" src="{{asset('shopifybot/img/tablescrapes.png')}}" alt="">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <img class="img-fluid" src="{{asset('shopifybot/img/tablescrapes.png')}}" alt="">
+            </div>
         </div>
     </div>
 @endif
@@ -66,14 +77,16 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 
 @yield('js')
 
 @if (session('success'))
     <script type="text/javascript">
-        $(document).ready(function(){
+        $(document).ready(function () {
             swal("Success!", "{{ session('success') }}", "success");
         });
     </script>
@@ -81,7 +94,7 @@
 
 @if (session('alert'))
     <script type="text/javascript">
-        $(document).ready(function(){
+        $(document).ready(function () {
             swal("Sorry!", "{{ session('alert') }}", "error");
         });
     </script>
